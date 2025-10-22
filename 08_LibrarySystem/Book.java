4a03b6db622b1b3a0812fcc9fb275e356b60fc6b
lilarosa <tt9898daa@gmail.com>
2025-10-22 10:50:46 +0200

    Create Book.java

---
+++ 08_LibrarySystem/Book.java
@@ -0,0 +1,23 @@
+/**
+ * Book：基本实体类
+ */
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle(){ return title; }
+    public String getAuthor(){ return author; }
+    public int getYear(){ return year; }
+
+    @Override
+    public String toString(){
+        return String.format("Book{title='%s', author='%s', year=%d}", title, author, year);
+    }
+}
